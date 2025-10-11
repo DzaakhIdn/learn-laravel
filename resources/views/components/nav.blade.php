aaaf493776fd8c0249419f489e7ef2d65e77ab25
DzaakhIdn <dzaakhidn@gmail.com>
2025-10-11 22:15:29 +0700

    pop

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800" x-data="{ isOpen: false }">
+<nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
@@ -8,13 +8,9 @@
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                        <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                        <a href="/" aria-current="page"
-                            class="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Home</a>
-                        <a href="/blog"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                        <a href="/about"
-                            class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                        <x-nav-link href="/" :current="request()->is('/')">Home</x-nav-link>
+                        <x-nav-link href="/blog" :current="request()->is('blog')">Blog</x-nav-link>
+                        <x-nav-link href="about" :current="request()->is('about')">About</x-nav-link>
                     </div>
                 </div>
             </div>
