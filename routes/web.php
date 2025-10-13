ed18275c98315548058e6b2591ba51411af20aa4
DzaakhIdn <dzaakhidn@gmail.com>
2025-10-13 13:14:28 +0700

    nambahin spasi

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Homepage']);
 });
+
 Route::get('/blog', function () {
     $posts = Post::all();
     return view('blog', ['title' => 'Blog Page', 'posts' => $posts]);
 });
+
 Route::get('/post/{post:slug}', function(Post $post){
     return view('post', ['title' => 'Single post', 'post' => $post]);
 });
+
 Route::get('/about', function () {
     return view('about', ['title' => 'About Page']);
 });
